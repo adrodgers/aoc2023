3b9b634ba5e6a18017f0c3fada60b6a6005abea8
Anthony Rodgers <a.d.rodgers@bham.ac.uk>
2023-12-04 09:17:39 +0000

    Day 4 part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,9 +1,4 @@
-const EXAMPLE_TEXT: &str = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+use std::collections::HashMap;
 
 struct Card {
     id: u32,
@@ -12,22 +7,23 @@ struct Card {
 }
 
 impl Card {
-    fn points(&self) -> u32 {
-        dbg!(self.id);
+    fn num_matches(&self) -> u32 {
         let mut num_matches = 0;
         for wnum in self.winning_numbers.iter() {
             for num in self.numbers.iter() {
                 if wnum == num {
                     num_matches += 1;
-                    dbg!(wnum, num, num_matches);
                 }
             }
         }
+        num_matches
+    }
+
+    fn points(&self) -> u32 {
         let mut out = 0;
-        if num_matches > 0 {
-            out = (2 as u32).pow(num_matches - 1);
+        if self.num_matches() > 0 {
+            out = 2_u32.pow(self.num_matches() - 1);
         }
-        dbg!(out);
         out
     }
 }
@@ -36,8 +32,9 @@ fn main() {
     let input1 = include_str!("./input1.txt");
     let output1 = part_1(input1);
     println!("{output1}");
-    // let input2 = include_str!("./input2.txt");
-    // part_2(input2);
+    let input2 = include_str!("./input1.txt");
+    let output2 = part_2(input2);
+    println!("{output2}");
 }
 
 fn part_1(input: &str) -> String {
@@ -52,7 +49,6 @@ fn part_2(input: &str) -> String {
 fn process_1(input: &str) -> String {
     let mut cards: Vec<Card> = Vec::new();
     for line in input.lines() {
-        dbg!(&line);
         let split: Vec<&str> = line.split(':').collect();
         let id: u32 = split[0]
             .split_whitespace()
@@ -62,19 +58,14 @@ fn process_1(input: &str) -> String {
             .parse()
             .unwrap();
         let card_numbers: Vec<&str> = split[1].split('|').collect();
-        dbg!(&card_numbers);
         let winning_numbers: Vec<u32> = card_numbers[0]
-            .trim()
             .split_whitespace()
             .map(|n| n.trim().parse().unwrap())
             .collect();
-        dbg!(&winning_numbers);
         let numbers: Vec<u32> = card_numbers[1]
-            .trim()
             .split_whitespace()
             .map(|n| n.trim().parse().unwrap())
             .collect();
-        dbg!(&numbers);
         cards.push(Card {
             id,
             winning_numbers,
@@ -86,12 +77,57 @@ fn process_1(input: &str) -> String {
 }
 
 fn process_2(input: &str) -> String {
-    "".to_string()
+    let mut cards: Vec<Card> = Vec::new();
+    let mut cards_map: HashMap<u32, u32> = HashMap::new();
+    for line in input.lines() {
+        let split: Vec<&str> = line.split(':').collect();
+        let id: u32 = split[0]
+            .split_whitespace()
+            .skip(1)
+            .take(1)
+            .collect::<Vec<&str>>()[0]
+            .parse()
+            .unwrap();
+        let card_numbers: Vec<&str> = split[1].split('|').collect();
+        let winning_numbers: Vec<u32> = card_numbers[0]
+            .split_whitespace()
+            .map(|n| n.trim().parse().unwrap())
+            .collect();
+        let numbers: Vec<u32> = card_numbers[1]
+            .split_whitespace()
+            .map(|n| n.trim().parse().unwrap())
+            .collect();
+        cards.push(Card {
+            id,
+            winning_numbers,
+            numbers,
+        });
+    }
+    for card in cards.iter() {
+        {
+            let num_copies = cards_map.entry(card.id).or_insert(0);
+            *num_copies += 1;
+        }
+        let num_copies = *cards_map.entry(card.id).or_insert(1);
+        let num_matches = card.num_matches();
+        for idx in 1..=num_matches {
+            let val = cards_map.entry(card.id + idx).or_insert(0);
+            *val += num_copies;
+        }
+    }
+    let out: u32 = cards_map.values().sum();
+    out.to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    const EXAMPLE_TEXT: &str = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
 
     #[test]
     fn test_1() {
@@ -101,6 +137,6 @@ mod tests {
     #[test]
     fn test_2() {
         let output = part_2(EXAMPLE_TEXT);
-        assert_eq!(output, "".to_string())
+        assert_eq!(output, "30".to_string())
     }
 }
