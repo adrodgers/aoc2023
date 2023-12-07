5ba45b65e4f539c26ca2564d13465b3382bb1af6
Anthony Rodgers <a.d.rodgers@bham.ac.uk>
2023-12-07 11:31:19 +0000

    Day 7 part 1

---
+++ day7/src/main.rs
@@ -0,0 +1,181 @@
+use std::{collections::HashMap, iter::zip, num::NonZeroI128};
+
+fn main() {
+    let input1 = include_str!("./input1.txt");
+    let output1 = part_1(input1);
+    println!("{output1}");
+    // let input2 = include_str!("./input2.txt");
+    // let output2 = part_2(input2);
+    // println!("{output2}");
+}
+
+// enum Rank {
+//     HighCard,
+//     OnePair,
+//     TwoPair,
+//     ThreeOfAKind,
+//     FullHouse,
+//     FourOfAKind,
+//     FiveOfAKind,
+// }
+
+const VALID_CARDS: [char; 13] = [
+    'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2',
+];
+
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Ord, Eq)]
+struct Hand {
+    strength: usize,
+    cards: [char; 5],
+    bet: u32,
+}
+
+impl Hand {
+    fn should_swap(&self, other: &Hand) -> bool {
+        if self.strength == other.strength {
+            for (c, other_c) in zip(self.cards, other.cards) {
+                if c != other_c {
+                    let val = VALID_CARDS.into_iter().rev().position(|r| r == c).unwrap();
+                    let other_val = VALID_CARDS
+                        .into_iter()
+                        .rev()
+                        .position(|r| r == other_c)
+                        .unwrap();
+                    // println!("{}:{}, {}:{}", c, val, other_c, other_val);
+                    if val < other_val {
+                        return false;
+                    } else if val > other_val {
+                        return true;
+                    } else {
+                        continue;
+                    }
+                }
+            }
+        }
+        false
+    }
+}
+
+fn hand_strength(cards: [char; 5]) -> usize {
+    let mut letter_counts: HashMap<char, u32> = HashMap::new();
+
+    let input_string: String = cards.iter().collect();
+    let char_vec: Vec<char> = input_string.chars().collect();
+    for c in char_vec {
+        *letter_counts.entry(c).or_insert(0) += 1;
+    }
+    let counts: Vec<u32> = letter_counts.into_values().collect();
+    // println!("{:?}", counts.clone().into_iter().max());
+    // println!("{:?}", counts.clone().into_iter().min());
+    // println!(
+    //     "{:?}",
+    //     counts.clone().into_iter().take_while(|c| *c == 2).count()
+    // );
+    match (
+        counts.clone().into_iter().max(),
+        counts.clone().into_iter().min(),
+        counts.clone().into_iter().filter(|c| *c == 2).count(),
+    ) {
+        // five of a kind
+        (Some(5), Some(5), _) => 6,
+        // four of a kind
+        (Some(4), Some(1), _) => 5,
+        // full house
+        (Some(3), Some(2), _) => 4,
+        // three of a kind
+        (Some(3), Some(1), _) => 3,
+        // two pair
+        (Some(2), Some(1), 2) => 2,
+        // one pair
+        (Some(2), Some(1), 1) => 1,
+        // high card
+        (Some(1), Some(1), _) => 0,
+        _ => 10,
+    }
+}
+
+fn part_1(input: &str) -> String {
+    process_1(input)
+}
+fn part_2(input: &str) -> String {
+    process_2(input)
+}
+
+fn process_1(input: &str) -> String {
+    let mut hands: Vec<Hand> = Vec::new();
+    for line in input.lines() {
+        let mut split = line.split_whitespace();
+        let cards: [char; 5] = split
+            .next()
+            .unwrap()
+            .chars()
+            .collect::<Vec<char>>()
+            .try_into()
+            .unwrap();
+        let bet = split.next().unwrap().parse::<u32>().unwrap();
+        let hand = Hand {
+            strength: hand_strength(cards),
+            cards,
+            bet,
+        };
+        //     dbg!(&hand);
+        hands.push(hand)
+    }
+    hands.sort();
+    let mut swap_idx: Option<usize> = None;
+    // dbg!(&hands.windows(2).len());
+    let mut index = 0;
+    while index + 1 != hands.len() {
+        for (idx, hand) in hands.windows(2).enumerate() {
+            index = idx + 1;
+            dbg!(index, hands.len());
+            // println!("idx:{}, hand:{:?}", idx, hand);
+            if hand[0].should_swap(&hand[1]) {
+                //             dbg!(&hand[0]);
+                //             dbg!(&hand[1]);
+                swap_idx = Some(idx);
+                break;
+            }
+            swap_idx = None;
+        }
+        if let Some(idx) = swap_idx {
+            // println!("inside swap");
+            //         // dbg!(&hands);
+            hands.swap(idx, idx + 1);
+            //         // dbg!(&hands);
+        }
+    }
+    // dbg!(&hands);
+    let out: u32 = hands
+        .iter()
+        .enumerate()
+        .map(|(rank, h)| (rank + 1) as u32 * h.bet)
+        .sum();
+
+    out.to_string()
+}
+
+fn process_2(input: &str) -> String {
+    "".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const EXAMPLE_TEXT: &str = "32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483";
+
+    #[test]
+    fn test_1() {
+        let output = part_1(EXAMPLE_TEXT);
+        assert_eq!(output, "6440".to_string())
+    }
+    #[test]
+    fn test_2() {
+        let output = part_2(EXAMPLE_TEXT);
+        assert_eq!(output, "".to_string())
+    }
+}
