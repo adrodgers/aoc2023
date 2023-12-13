454c5bc2a97224049f46c01f264f867e6a93aca3
Anthony Rodgers <a.d.rodgers@bham.ac.uk>
2023-12-13 10:06:08 +0000

    Day 13 part 2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,12 +1,12 @@
-use itertools::Itertools;
+// use itertools::Itertools;
 
 fn main() {
     let input1 = include_str!("./input1.txt");
     let output1 = part_1(input1);
     println!("{output1}");
-    // let input2 = include_str!("./input2.txt");
-    // let output2 = part_2(input2);
-    // println!("{output2}");
+    let input2 = include_str!("./input1.txt");
+    let output2 = part_2(input2);
+    println!("{output2}");
 }
 
 fn part_1(input: &str) -> String {
@@ -18,13 +18,25 @@ fn part_2(input: &str) -> String {
 
 fn process_1(input: &str) -> String {
     let puzzles = Puzzle::parse_all(input);
-    dbg!(&puzzles);
     let mut sum = 0;
     for puzzle in puzzles {
         sum += puzzle.horizontal_reflection();
         sum += puzzle.vertical_reflection();
     }
 
+    sum.to_string()
+}
+
+fn process_2(input: &str) -> String {
+    let puzzles = Puzzle::parse_all(input);
+    let mut sum = 0;
+    for puzzle in puzzles {
+        let h = puzzle.horizontal_reflection_p2();
+        sum += h;
+        if h == 0 {
+            sum += puzzle.vertical_reflection_p2();
+        }
+    }
     sum.to_string()
 }
 
@@ -53,23 +65,18 @@ impl Puzzle {
 
     fn horizontal_reflection(&self) -> usize {
         let mut reflection_index = 0;
-        for idx in (1..self.nrows) {
-            dbg!(&idx);
+        for idx in 1..self.nrows {
             let left_iter = (0..idx).rev();
-            let right_iter = (idx..self.nrows);
-            dbg!(&left_iter);
-            dbg!(&right_iter);
+            let right_iter = idx..self.nrows;
 
             if left_iter
                 .zip(right_iter)
-                .inspect(|x| println!("{:?}", x))
                 .all(|row| self.data[row.0] == self.data[row.1])
             {
                 reflection_index = idx * 100;
                 break;
             }
         }
-        dbg!(&reflection_index);
 
         reflection_index
     }
@@ -79,30 +86,85 @@ impl Puzzle {
         let data_transform: Vec<Vec<char>> = (0..self.ncols)
             .map(|col| self.data.iter().map(|row| row[col]).collect())
             .collect();
-        for idx in (1..self.ncols) {
-            dbg!(&idx);
+        for idx in 1..self.ncols {
             let left_iter = (0..idx).rev();
-            let right_iter = (idx..self.ncols);
-            dbg!(&left_iter);
-            dbg!(&right_iter);
+            let right_iter = idx..self.ncols;
 
             if left_iter
                 .zip(right_iter)
-                .inspect(|x| println!("{:?}", x))
                 .all(|col| data_transform[col.0] == data_transform[col.1])
             {
                 reflection_index = idx;
                 break;
             }
         }
-        dbg!(&reflection_index);
 
         reflection_index
     }
-}
 
-fn process_2(input: &str) -> String {
-    "".to_string()
+    fn horizontal_reflection_p2(&self) -> usize {
+        let original_reflection_horizontal = self.horizontal_reflection();
+
+        let mut reflection_index = 0;
+        'outer: for y in 0..self.nrows {
+            for x in 0..self.ncols {
+                let mut new_data = self.data.clone();
+                let c = new_data[y][x];
+                match c {
+                    '#' => new_data[y][x] = '.',
+                    '.' => new_data[y][x] = '#',
+                    _ => panic!("invalid char"),
+                };
+                for idx in 1..self.nrows {
+                    let left_iter = (0..idx).rev();
+                    let right_iter = idx..self.nrows;
+
+                    if left_iter
+                        .zip(right_iter)
+                        .all(|row| new_data[row.0] == new_data[row.1])
+                        && (idx * 100) != original_reflection_horizontal
+                    {
+                        reflection_index = idx * 100;
+                        break 'outer;
+                    }
+                }
+            }
+        }
+        reflection_index
+    }
+
+    fn vertical_reflection_p2(&self) -> usize {
+        let original_reflection_vertical = self.vertical_reflection();
+        let mut reflection_index = 0;
+        'outer: for y in 0..self.nrows {
+            for x in 0..self.ncols {
+                let mut new_data = self.data.clone();
+                let c = new_data[y][x];
+                match c {
+                    '#' => new_data[y][x] = '.',
+                    '.' => new_data[y][x] = '#',
+                    _ => panic!("invalid char"),
+                };
+                let data_transform: Vec<Vec<char>> = (0..self.ncols)
+                    .map(|col| new_data.iter().map(|row| row[col]).collect())
+                    .collect();
+                for idx in 1..self.ncols {
+                    let left_iter = (0..idx).rev();
+                    let right_iter = idx..self.ncols;
+
+                    if left_iter
+                        .zip(right_iter)
+                        .all(|col| data_transform[col.0] == data_transform[col.1])
+                        && idx != original_reflection_vertical
+                    {
+                        reflection_index = idx;
+                        break 'outer;
+                    }
+                }
+            }
+        }
+        reflection_index
+    }
 }
 
 #[cfg(test)]
@@ -132,6 +194,6 @@ mod tests {
     #[test]
     fn test_2() {
         let output = part_2(EXAMPLE_TEXT);
-        assert_eq!(output, "".to_string())
+        assert_eq!(output, "400".to_string())
     }
 }
